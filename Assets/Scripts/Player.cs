30242f8fa219661c7a6127ecb6585757fb86532e
Aditivo-rgb <aida.hagag062@comunidadunir.net>
2025-04-20 13:05:26 +0200

    Pickups

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,11 +25,31 @@ public class Player : MonoBehaviour, Danhable
     private Vector3 velocidadVertical;
 
    [Header ("Sistema de combate")]
-   [SerializeField] private float vidas = 100f;
+   [SerializeField] private float vidas = 0f;
+   [SerializeField] private float vidasMax = 100f;
    [SerializeField] private float distanciaDisparo = 500;
    [SerializeField] private float danhoDisparo = 20;
 
-   private AudioSource audioSource;
+    [Header("Sistema de sonido")]
+    [SerializeField] private AudioSource audiodisparo;
+    [SerializeField] private AudioSource audiopasos;
+    [SerializeField] private AudioSource audiogatillo;
+    [SerializeField] private AudioSource[] audioPasos;
+    [SerializeField] private float intervaloPasos = 0.5f;
+    [SerializeField] private AudioSource audioReload;
+    [SerializeField] private AudioSource audioHit;
+    private float timerPasos = 0f;
+    private bool meMuevo = false;
+    
+
+    private AudioSource audioSource;
+   
+    //Inventario
+   private int ammo = 0;
+   private int maxAmmo = 50;
+   private int ammoClip = 0;
+   private int ammoClipMax = 10;
+   
 
     bool cursorIsLocked = true;
     bool lockCursor = true;
@@ -48,6 +68,10 @@ public class Player : MonoBehaviour, Danhable
         Cursor.lockState = CursorLockMode.Locked;
         controller = GetComponent<CharacterController>();
         audioSource = GetComponent<AudioSource>();
+
+        // Poner vidas a tope
+
+        vidas = vidasMax; 
     }
 
     // Update is called once per frame
@@ -91,6 +115,22 @@ public class Player : MonoBehaviour, Danhable
         direccionMovimiento = camara.forward * direccionInput.z + camara.right * direccionInput.x;
         direccionMovimiento.y = 0;//porque esto es la gravedad
         controller.Move(direccionMovimiento * velocidadMovimiento * Time.deltaTime);//hacia donde * cuán rápido * el tiempo que dure el movimiento
+        //solo suenan los pasos si hay movimiento
+        meMuevo = direccionInput.magnitude > 0.1f;
+        if (meMuevo) 
+        {
+            timerPasos -= Time.deltaTime;//¿ya he pasado el tiempo desde el último paso?
+            if (timerPasos <= 0f)//si ha pasado
+            {
+                HacerSonarPasos();//suenan mis pasos
+                timerPasos = intervaloPasos;//reinicio el timer cada tiempo que le diga
+            }
+        }
+        else
+        {
+            timerPasos = 0f;//si no me muevo reinicio
+        }
+        
     }
 
     private void ManejarVelocidadVertical()
@@ -134,55 +174,100 @@ public class Player : MonoBehaviour, Danhable
         Quaternion rotacionObjetivo = Quaternion.LookRotation(direccionMovimiento);
         transform.rotation = rotacionObjetivo;
     }
+    
     //Combate
     public void RecibirDahno(float danho)
     {
         vidas -= danho;
         if (vidas <= 0)
         {
-            Destroy(this.gameObject);
+            audioHit.Play();
+        }
+    }
+
+    private void Disparar()
+    {
+        if (ammoClip > 0)
+        { 
+            anim.SetTrigger("shoot");
+            //meto el disparo para que pase a la vez que el sonido
+            ammoClip--;
+            Debug.Log("Ammo left" + ammo);
+            audiodisparo.Play();
+            particles.Play();
+        
+
+            //lo encapsulo dentro de un if porque me devuelve un bool
+            //mira a ver si impactas en algo...
+            if(Physics.Raycast(camara.position, camara.forward, out RaycastHit hitInfo, distanciaDisparo))//origen, dirección, distancia y qué hemos tocado (acepción 12)
+            {
+                    //Saber si el gameObject con el que he colisionado tiene la interfaz buscada
+                    //mira a ver si ese algo es dañable
+                    if (hitInfo.transform.TryGetComponent (out Danhable sistemaDanho))
+                    {
+                        if(!hitInfo.transform.CompareTag("Player"))
+                        {
+                            sistemaDanho.RecibirDahno(danhoDisparo);
+                            Debug.Log("Estás dando");
+                        }
+                     
+                   
+                    }
+
+            }
+                
+        }         
+            
+        else
+        {
+            anim.SetTrigger("noBullets");
+            audiogatillo.Play();
         }
     }
     private void Recargar()
     {
-        anim.SetTrigger("reload");
+        anim.SetTrigger("reload");//animacion de recarga
+        audioReload.Play();
+        int amountNeeded = ammoClipMax - ammoClip;//cuantas balas necesitamos para llenar el cargador
+        int ammoAvaliable = amountNeeded < ammo ? amountNeeded : ammo;//cuantas balas podemos usar para recargar
+        ammo -= ammoAvaliable;//resto las balas que uso de la reserva
+        ammoClip += ammoAvaliable;//sumo estas balas al cargador
     }
-    private void Disparar()
-    {
-        anim.SetTrigger("shoot");
-        audioSource.Play();
-        particles.Play();
-        //lo encapsulo dentro de un if porque me devuelve un bool
-        //mira a ver si impactas en algo...
-        if(Physics.Raycast(camara.position, camara.forward, out RaycastHit hitInfo, distanciaDisparo))//origen, dirección, distancia y qué hemos tocado (acepción 12)
-        {
-                //Saber si el gameObject con el que he colisionado tiene la interfaz buscada
-                //mira a ver si ese algo es dañable
-                if (hitInfo.transform.TryGetComponent (out Danhable sistemaDanho))
-                {
-                    if(!hitInfo.transform.CompareTag("Player"))
-                    {
-                        sistemaDanho.RecibirDahno(danhoDisparo);
-                        Debug.Log("Estás dando");
-                    }
-                     
-                   
-                }
-        }
-                
-            
-        
-    }
+    
     //Recoger
-
     void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.tag == "Ammo")
+        //Munición
+       if (collider.gameObject.CompareTag("Ammo") && ammo < maxAmmo)
+       {
+            ammo += Mathf.Clamp(ammo + 10, 0, maxAmmo);
+            Debug.Log("Ammo" + ammo);
+            //Sonido
+            AudioSource audioAmmo = collider.gameObject.GetComponent<AudioSource>();
+            if (audioAmmo != null && audioAmmo.clip != null)
+            {
+                AudioSource.PlayClipAtPoint(audioAmmo.clip, collider.transform.position);
+            }
+
+            Destroy(collider.gameObject);
+               
+       }
+        
+        //Botiquines
+        else if (collider.gameObject.CompareTag("Medkit") && vidas < vidasMax)
         {
-            AudioSource audioAmmo = collider.GetComponent<AudioSource>();
-            audioAmmo.Play();
+            vidas = Mathf.Clamp(vidas + 20, 0, vidasMax);
+            Debug.Log("Medkit:" + vidas);
+        //Sonido
+            AudioSource audioMedkit = collider.gameObject.GetComponent<AudioSource>();
+            if (audioMedkit != null && audioMedkit.clip != null)
+            {
+                    AudioSource.PlayClipAtPoint(audioMedkit.clip, collider.transform.position);
+            }
+
             Destroy(collider.gameObject);
         }
+        
     }
 
     //Ratón
@@ -225,6 +310,19 @@ public class Player : MonoBehaviour, Danhable
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
+    }
+
+    void HacerSonarPasos()
+    {
+        if (audioPasos.Length == 0) return;//tiene sonidos mi array? si no, no ejecutes
+
+        int n = Random.Range(1, audioPasos.Length);//cojo un número aleatorio entre 1 y mi último índice
+        audioPasos[n].Play();//suenan los pasos
+
+        // Mezclamos el orden para evitar que se repita el mismo clip
+        AudioSource temp = audioPasos[n];//el sonido se almacena en n
+        audioPasos[n] = audioPasos[0]; // muevo el último sonido al 0 
+        audioPasos[0] = temp; // para que no suene otra vez de inmediato
     }
 
     private void OnDrawGizmos()
