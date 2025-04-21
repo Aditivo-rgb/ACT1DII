9e336e27735bd1e62e7c6ccb9510a637dd16cf35
Aditivo-rgb <aida.hagag062@comunidadunir.net>
2025-04-21 19:20:03 +0200

    Finite State Machine completa

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -8,10 +8,17 @@ public class Enemigo : MonoBehaviour, Danhable
     private Animator anim;
     private float vidas = 100;
 
+    [Header("Sistema de movimiento")]
+    [SerializeField] private float walkingSpeed;
+    [SerializeField] private float runningSpeed;
+
     [Header("Sistema de combate")]
     [SerializeField] private Transform puntoAtaque;
     [SerializeField] private float radioAtaque;
     [SerializeField] private float danhoAtaque = 20;
+
+    enum STATE { IDLE, WANDER, ATTACK, CHASE, DEAD}
+    STATE state = STATE.IDLE;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,31 +26,127 @@ public class Enemigo : MonoBehaviour, Danhable
         target = FindObjectOfType<Player>();//le pido que cuando inicie localice cual es el objeto con player
         anim = GetComponent<Animator>();
 
-        anim.SetBool("isWalking", true);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.transform.position);//encuentra el sitio donde está el player aunque se mueva
-        
+        //agent.SetDestination(target.transform.position);//encuentra el sitio donde está el player aunque se mueva
         //Distancia de ataque
-        if(agent.remainingDistance <= agent.stoppingDistance)
+        //if(agent.remainingDistance <= agent.stoppingDistance)
+        //{
+            //EnfocarObjetivo();
+            //LanzarAtaque();
+        //}
+        
+        switch (state)
         {
-            EnfocarObjetivo();
-            LanzarAtaque();
+            case STATE.IDLE:
+                if (CanSeePlayer()) state = STATE.CHASE;
+                else if (Random.Range(0,5000) < 5)
+                state = STATE.WANDER;
+                break;
+            case STATE.WANDER:
+                Wander();
+                if (CanSeePlayer()) state = STATE.CHASE;
+                else if (Random.Range(0, 5000) < 5)
+                {
+                    state = STATE.IDLE;
+                    TurnOffTriggers();
+                    agent.ResetPath();
+                }
+                    break;
+            case STATE.CHASE:
+                Chase();
+
+                if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+                {
+                    state = STATE.ATTACK;
+                }
+
+                if(ForgetPlayer())
+                {
+                    state = STATE.WANDER;
+                    agent.ResetPath();
+                }
+                break;
+            case STATE.ATTACK:
+                TurnOffTriggers();
+                LanzarAtaque();
+                EnfocarObjetivo();
+
+                if (DistanceToPlayer() > agent.stoppingDistance + 2)
+                {
+                    agent.isStopped = false;
+                    state = STATE.CHASE;
+                }
+                    
+                break;
+            case STATE.DEAD:
+                break;
         }
 
     }
 
+    private void Chase()
+    {
+        agent.SetDestination(target.transform.position);
+        agent.stoppingDistance = 2;
+        TurnOffTriggers();
+        agent.speed = runningSpeed;
+        anim.SetBool("isRunning", true);
+    }
+
+    //State machine
+    private void Wander()
+    {
+        if (!agent.hasPath)
+        {
+            float newX = this.transform.position.x + Random.Range(-5, 5);
+            float newZ = this.transform.position.z + Random.Range(-5, 5);
+            Vector3 terrainPos = Terrain.activeTerrain.GetPosition();
+            float newY = Terrain.activeTerrain.SampleHeight(new Vector3(newX, 0, newZ)) + terrainPos.y;
+            Vector3 dest = new Vector3(newX, newY, newZ);
+            agent.SetDestination(dest);
+            agent.stoppingDistance = 0;
+            TurnOffTriggers();
+            agent.speed = walkingSpeed;
+            anim.SetBool("isWalking", true);
+
+        }
+    }
+    float DistanceToPlayer()
+    {
+        return Vector3.Distance(target.transform.position, this.transform.position);
+    }
+    bool CanSeePlayer()
+    {
+        if(DistanceToPlayer() < 10)
+            return true;
+        return false;
+    }
+    bool ForgetPlayer() 
+    {
+        if (DistanceToPlayer() > 20)
+            return true;
+        return false;
+    }
+    void TurnOffTriggers()
+    {
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isAttacking", false);
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isDead", false);
+    }
+
+    //Combate
     private void LanzarAtaque()
     {
         agent.isStopped = true;
-        anim.SetBool("isWalking", false);
-        Debug.Log("Me paro");
+
         anim.SetBool("isAttacking", true);
     }
-
     private void Atacar()
     {
         //Lanzo el overlap desde un punto de ataque y  bajo un radio de ataque y recojo todos los collider tocados
@@ -56,7 +159,6 @@ public class Enemigo : MonoBehaviour, Danhable
             }
         }
     }
-
     private void EnfocarObjetivo()
     {
         //Si tienes un objetivo B y quieres sacar la dirección desde el punto A siempre restas destino - origen y normalizas
@@ -67,7 +169,6 @@ public class Enemigo : MonoBehaviour, Danhable
         Quaternion rotacionAObjetivo = Quaternion.LookRotation(direccionAObjetivo);
         transform.rotation = rotacionAObjetivo;
     }
-
     private void FinDeAtaque()
     {
         agent.isStopped = false;
